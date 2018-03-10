91d6f7cf5b208a9bf41a0539b296c2d8f74d0f37
Kevin J Donohue <kevinjdonohue@yahoo.com>
2018-03-10 01:07:31 -0800

    Setting up es6 transpilation, linting, and executing tests

---
+++ src/SideEffects/side-effects.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+let y;
+let z;
+
+export default function foo(x) {
+  y = x * 2;
+  z = x * 3;
+}
---
+++ src/SideEffects/side-effects_test.js
@@ -0,0 +1,5 @@
+describe('Side Effects Tests', () => {
+  it('true should be true', () => {
+    true.should.equal(true);
+  });
+});
